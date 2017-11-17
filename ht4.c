06728ba71a322b1192c79b9b816eb7b218c90712
Egor <31863304+EgorOrachyov@users.noreply.github.com>
2017-11-17 17:10:06 +0300

    Strings

--- ht4.c
+++ ht4.c
@@ -6,45 +6,70 @@ int max(int a, int b) {
 }
 
 // return length of string
-int strlen(char *s) {
- 	int i = 0; 
- 	while (s[i] != '\0') {
+int strlen(char * s) {
+	int i = 0; 
+	while (s[i] != '\0') {
 		i++;
- 	}
- 	return i;
+	}
+	return i;
 }
 
 // copy string source to string destination
-void strcpy(char *dst, char *src) {
-	int length = strlen(src);
-	
-	for(int i = 0; i < length + 1; i++){
+void strcpy(char * dst, char * src) {	
+	int i = 0;
+	while (src[i] != '\0') {
 		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = src[i];
 }
 
 // concatenation of strings destination and source
-void strcat(char *dst, char *src) {
-	int dstLen = strlen(dst); 
-	int srcLen = strlen(src); 
-
-	for (int i = 0; i < srcLen + 1; i++) {
-		dst[dstLen + i] = src[i];
+void strcat(char * dst, char * src) {
+	int i = 0;
+	while (dst[i] != '\0') {
+		i++;
 	}
+	
+	int j = 0;
+	while (src[j] != '\0') {    	
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = src[j];
+
 }
 
 // comparison of strings s1 and s1: return 0 - are equal, <0 - first is smaller, >0 - first is bigger
-int strcmp(char *s1, char *s2) {
+int strcmp(char * s1, char * s2) {
 	int i = 0;
-	int length = max(strlen(s1), strlen(s2));
- 
-	while ((s1[i] - s2[i] == 0) && (i < length)) {
+	while ((s1[i] - s2[i] == 0) && (s1[i] != '\0') && (s2[i] != '\0')) {
 	  i++;
 	}
 	return s1[i] - s2[i];
 }
 
 int main() {
+   
+    /*
 
+	char s1[] = "abcd";
+	char s2[] = "abcde";
+	char s3[] = " hhhhh";
+	char s4[] = "abcde";
+
+
+	printf("%i \n", strlen(s1));
+
+	strcpy(s2, s1);
+	printf("%s \n", s2);
+
+	strcat(s2, s3);
+	printf("%s \n", s2);
+
+	printf("%i \n", strcmp(s1, s4));
+
+	*/
+	
 	return 0;
 }
